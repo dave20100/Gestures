b3f4b1b8d210f3b9ad285b794edfaf42c8719e89
Dawid <dave201000@gmail.com>
2018-10-08 17:54:08 +0200

    First commit just created bufor

---
+++ Gestures/gestureField.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Gestures
+{
+    class gestureField
+    {
+        string gestureCodeBufor = "";
+        Canvas paintField;
+        List<List<gesturePoint>> pointField;
+
+        public gestureField(Canvas gestureCanv)
+        {
+            pointField = new List<List<gesturePoint>>();
+            this.paintField = gestureCanv;
+            fillField(3);
+        }
+
+        private void fillField(int size)
+        {
+            this.pointField.Clear();
+            for(int i = 0; i < size; i++)
+            {
+                List<gesturePoint> tmp = new List<gesturePoint>();
+                for (int j = 0; j < size; j++)
+                {
+                    gesturePoint pointToAdd = new gesturePoint(100, "[" + i + "," + j + "]");
+                    pointToAdd.IsLookedAt += PointToAdd_IsLookedAt;
+                    tmp.Add(pointToAdd);
+                }
+                this.pointField.Add(tmp);
+            }
+            drawField();
+        }
+
+        private void PointToAdd_IsLookedAt(object sender, EventArgs e)
+        {
+            if(gestureCodeBufor.Length >= pointField.Count * pointField[0].Count * 5)
+            {
+                gestureCodeBufor = gestureCodeBufor.Remove(0, 5);
+            }
+            gestureCodeBufor += ((gesturePoint)sender).id;
+            
+        }
+
+        private void drawField()
+        {
+            this.paintField.Children.Clear();
+            for (int i = 0; i < pointField.Count; i++)
+            {
+                for (int j = 0; j < pointField[0].Count; j++)
+                {
+                    double distX = paintField.ActualWidth / pointField.Count;
+                    double distY = paintField.ActualHeight / pointField[0].Count;
+                    // wzor (polowa ekranu + (ekran / ilosc) * i i j :D 
+                    pointField[i][j].Draw(distX / 2 + distX * i, distY / 2 + distY * j , this.paintField);
+                }
+            }
+        }
+
+        public void changeSize(int size)
+        {
+            fillField(size);
+        }
+        
+    }
+}
---
+++ Gestures/gesturePoint.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace Gestures
+{
+    class gesturePoint  : UIElement
+    {
+        public event EventHandler IsLookedAt;
+        SolidColorBrush hovered = new SolidColorBrush(Colors.Red);
+        SolidColorBrush idle = new SolidColorBrush(Colors.Black);
+        Ellipse center;
+        Ellipse outer;
+        int size;
+        public string id;
+        
+        public gesturePoint(int size, string id)
+        {
+            this.size = size;
+            this.id = id;
+
+            center = new Ellipse()
+            {
+                Width = size / 10,
+                Height = 10,
+                Fill = idle,
+            };
+            outer = new Ellipse()
+            {
+                Width = size,
+                Height = size,
+                Fill = idle,
+                Opacity = 0.3
+            };
+            center.IsMouseDirectlyOverChanged += MouseOverChanged;
+            outer.IsMouseDirectlyOverChanged += MouseOverChanged;
+        }
+
+        private void MouseOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if((bool)e.NewValue == true)
+            {
+                center.Fill = hovered;
+                IsLookedAt?.Invoke(this, EventArgs.Empty);
+            }
+            else
+            {
+                center.Fill = idle;
+            }
+        }
+
+        public void Draw(double x, double y, Canvas toDrawOn)
+        {
+            Canvas.SetLeft(center , x - (10/2));
+            Canvas.SetTop(center, y - (10/2));
+            Canvas.SetLeft(outer, x - this.size/2);
+            Canvas.SetTop(outer, y - this.size/2);
+            toDrawOn.Children.Add(outer);
+            toDrawOn.Children.Add(center);
+        }
+        
+    }
+}
